d1ac21ad34fc69a32dca09b65732a49fd4c161cf
Eby-Samuel693 <coquelet.samuel@gmail.com>
2021-06-03 18:35:30 +0200

    img & frontImg!!!

--- View/_partials/galerie.php
+++ View/_partials/galerie.php
@@ -1,18 +1,35 @@
 <?php
     include 'head.php';
-    require $_SERVER['DOCUMENT_ROOT'] . '/View/_partials/pic/uploadImage.php';
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/Classes/DB.php';
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/View/_partials/pic/uploadImage.php';
+    use MinichatProjet\Classes\DB;
+use MinichatProjet\Entity\pic;
+
 ?>
 <h5> Galerie d'image </h5>
 
 <div id="galerieForm">
-    <form method="POST" action="pic/uploadImage.php" enctype="multipart/form-data">
-        <input type="text" name="imgName" placeholder="nom de l'image" class="fichier">
+    <form method="POST" action="pic/uploadImage.php" enctype="multipart/form-data" class="formFile">
+        <input type="text" name="imgName" placeholder="nom de l'image" class="fichier"><br>
         <input type="file" name="img" id="img" class="fichier" />&nbsp;( Max : 8Mo)<br>
         <div><input type="submit" class="btn fichier" value="envoyer" /></div>
         <input type="hidden" value="b" name="env"/>
     </form>
     <div id="lookImg">
         <?php
+            $request = DB::connectDB()->prepare("SELECT * FROM pic");
+            $request->execute();
+
+            $images = $request->fetchAll();
+            foreach ($images as $image){
+                $src = '/View/_partials/pic/' . $image['img'];
+                //if(file_exists($src)){?>
+                    <div class="fileDiv">
+                        <img class="small-img" alt="<?= $image['img'] ?>" src="<?= $src ?>">
+                    </div><?php
+
+                //}
+            }
 
         ?>
     </div>
--- View/_partials/pic/uploadImage.php
+++ View/_partials/pic/uploadImage.php
@@ -1,7 +1,10 @@
 <?php
 
-use MinichatProjet\Classes\DB;
+require_once $_SERVER['DOCUMENT_ROOT'] . '/Classes/DB.php';
 require_once $_SERVER["DOCUMENT_ROOT"] . '/Modele/Entity/pic.php';
+
+use MinichatProjet\Classes\DB;
+
 
 
 /**
@@ -22,7 +25,7 @@ function getRandomName(String $nameRan): string {
 
 if (isset($_FILES["img"]) && $_FILES["img"]["error"] === 0) {
 
-    $allowedMimeType = ['text/plain', 'image/jpeg', 'image/jpeg'];
+    $allowedMimeType = ['text/plain', 'image/jpeg', 'image/jpg'];
     if(in_array($_FILES['img']['type'], $allowedMimeType)) {
 
         $maxSize = 8 * 4096 * 4096; // = 8 mo
@@ -34,9 +37,12 @@ if (isset($_FILES["img"]) && $_FILES["img"]["error"] === 0) {
             move_uploaded_file($tmp_name, '../pic/' . $name);
 
             $request = DB::connectDB()->prepare("INSERT INTO pic (img) VALUES ( :img)");
-            $request->execute(array($name));
+            $request->bindValue(':img',$name);
+            $request->execute();
 
             header('location: ../galerie.php');
+
+
         }
         else {
             echo ' le fichier  est trop volumineux';
